b32d1fcf3e47fe7d74dc48150e4b859b6fa7b469
Adam Spickard <spickard.adam@gmail.com>
2020-06-08 08:30:32 -0400

    Fix broken router generator

--- generator/templates/simple/src/router.js
+++ generator/templates/simple/src/router.js
@@ -3,14 +3,13 @@ extend: '@vue/cli-service/generator/router.js'
 replace:
   - !!js/regexp /import Vue from 'vue'/
   - !!js/regexp /import Router from 'vue-router'/
-  - !!js/regexp /Vue.use\(Router\)/
-  - !!js/regexp /export default new Router\(\{/
   - !!js/regexp /import Home from '\./views/Home.vue'/
+  - !!js/regexp /Vue.use\(Router\)/
+
   - !!js/regexp /\(\) => import(.*)\.\/views\/About\.vue'\)/
-  - !!js/regexp /(\s+)\/\/ (.*)/
-  - !!js/regexp /(\s+)\/\/ (.*)/
-  - !!js/regexp /(\s+)\/\/ (.*)/
+  - !!js/regexp /const router = new VueRouter\(\{/
   - !!js/regexp /\}\)/
+  - !!js/regexp /export default router/
 ---
 
 <%# REPLACE %>
@@ -22,7 +21,16 @@ import Router from 'vue-router';
 <%# END_REPLACE %>
 
 <%# REPLACE %>
+import Home from '~/views/Home.vue';
+import About from '~/views/About.vue';
+<%# END_REPLACE %>
+
+<%# REPLACE %>
 Vue.use(Router);
+<%# END_REPLACE %>
+
+<%# REPLACE %>
+About
 <%# END_REPLACE %>
 
 <%# REPLACE %>
@@ -30,24 +38,9 @@ export const options = {
 <%# END_REPLACE %>
 
 <%# REPLACE %>
-import Home from '~/views/Home.vue';
-import About from '~/views/About.vue';
-<%# END_REPLACE %>
-
-<%# REPLACE %>
-About,
-<%# END_REPLACE %>
-
-<%# REPLACE %>
-<%# END_REPLACE %>
-
-<%# REPLACE %>
-<%# END_REPLACE %>
-
-<%# REPLACE %>
-<%# END_REPLACE %>
-
-<%# REPLACE %>
 };
 export default new Router(options);
+<%# END_REPLACE %>
+
+<%# REPLACE %>
 <%# END_REPLACE %>
